62f60b3ae5185658dfa53cb9ba3f9f8200c096f2
Thomas Sickert <thomas.sickert@gmail.com>
2020-03-22 23:43:22 -0700

    updated dockerfile

--- flatbuffers/dockerize.sh
+++ flatbuffers/dockerize.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Install flatbuffers
-git clone https://github.com/google/flatbuffers.git
-cd flatbuffers
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
-make
-
-# Build docker image
 docker build -t flatc .
