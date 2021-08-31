f6de3167281d6d4626ee1597c4b92372016a6218
Thomas Sickert <thomas.sickert@gmail.com>
2021-08-30 22:50:01 -0700

    Remove goswagger since the project now has a dockerfile

--- scripts/install_goswagger.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt install gnupg ca-certificates
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
-echo "deb https://dl.bintray.com/go-swagger/goswagger-debian ubuntu main" | tee /etc/apt/sources.list.d/goswagger.list
-apt update 
-apt install swagger
